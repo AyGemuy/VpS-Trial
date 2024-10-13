a5d8bd89a5a6a5f1cfe1b29f6aa9d80bc96c7192
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2024-10-13 22:58:39 +0800

    Add files via upload

--- api/run.js
+++ api/run.js
@@ -1,20 +1,12 @@
-const express = require('express');
 const shell = require('shelljs');
 
-// Inisialisasi express
-const app = express();
-
-// Route untuk mengeksekusi perintah shell
-app.get('/run', (req, res) => {
-  shell.exec('curl -sSL https://dokploy.com/install.sh | sh', (code, stdout, stderr) => {
-    if (code !== 0) {
-      console.error(`Error: ${stderr}`);
-      return res.status(500).json({ message: `Error executing command: ${stderr}` });
-    }
-    console.log(`Stdout: ${stdout}`);
-    res.json({ message: `Command executed successfully`, output: stdout });
-  });
+// Eksekusi perintah shell
+shell.exec('curl -sSL https://dokploy.com/install.sh | sh', (code, stdout, stderr) => {
+  if (code !== 0) {
+    console.error(`Error: ${stderr}`);
+    process.exit(1); // Keluar dengan kode kesalahan
+  }
+  
+  console.log(`Stdout: ${stdout}`);
+  process.exit(0); // Keluar dengan sukses
 });
-
-// Handler untuk Vercel
-module.exports = app;
